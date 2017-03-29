ce88326ba37224ea433de8fb3f71fa64a0098af2
dimysiklapo4ka <dimysiklapo4ka@gmail.com>
2017-03-30 02:58:58 +0300

    v-0.1

--- Anogramm.java
+++ Anogramm.java
@@ -1,7 +1,32 @@
 package InitDnepr.home_work;
 
+import java.util.Scanner;
+
+import static com.sun.org.apache.xalan.internal.lib.ExsltStrings.split;
+
 /**
  * Created by d1mys1klapo4ka on 27.03.2017.
  */
 public class Anogramm {
+
+    static Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) {
+
+        String getStringText = getString();
+        String[] anogram = getStringText.split(" ");
+
+        for (int i = 0, n = anogram.length; i < n; i++){
+            for (int j = 0; j < n; j++){
+                if (anogram[i].equals(anogram[j])){
+                    System.out.println(anogram[i] + anogram[j]);
+                }
+            }
+        }
+    }
+
+    static String getString(){
+        String getStringText = scanner.nextLine();
+
+        return getStringText;
+    }
 }
--- StartString.java
+++ StartString.java
@@ -4,4 +4,35 @@ package InitDnepr.home_work;
  * Created by d1mys1klapo4ka on 27.03.2017.
  */
 public class StartString {
+
+    public static void main(String[] args) {
+
+        String a = "мама ";
+        String b = "мыла ";
+        String c = "раму";
+
+        int lenghtA = a.length();
+        System.out.println(lenghtA);
+
+        String abc = a.concat(b).concat(c);
+        System.out.println(abc);
+
+        float flot = (float) 5.3;
+        int Int = 34;
+        double Dob = 2.4;
+
+//        System.out.printf("Значение переменной float = %f " +
+//                "пока значение integer = %d " +
+//                "переменная с плавающей точкой = %d, и string = %s" +
+//                flot, Int, Dob, abc);
+
+        String fs;
+        fs = String.format("Значение переменной float = " +
+                "%f, пока значение integer " +
+                "переменная = %d, и string " +
+                "= %s" + " double = %f", flot, Int, abc, Dob);
+        System.out.println(fs);
+
+
+    }
 }
