bd352f7815223a571c1868fd6f5365f480536117
dimysiklapo4ka <dimysiklapo4ka@gmail.com>
2017-04-01 02:55:38 +0300

    anogram is done

--- Anogramm.java
+++ Anogramm.java
@@ -1,32 +1,33 @@
 package InitDnepr.home_work;
 
+import java.util.Arrays;
 import java.util.Scanner;
-
-import static com.sun.org.apache.xalan.internal.lib.ExsltStrings.split;
 
 /**
  * Created by d1mys1klapo4ka on 27.03.2017.
  */
 public class Anogramm {
 
-    static Scanner scanner = new Scanner(System.in);
     public static void main(String[] args) {
 
-        String getStringText = getString();
-        String[] anogram = getStringText.split(" ");
+        Scanner scanner = new Scanner(System.in);
+        String getStringText = scanner.nextLine();
 
-        for (int i = 0, n = anogram.length; i < n; i++){
-            for (int j = 0; j < n; j++){
-                if (anogram[i].equals(anogram[j])){
-                    System.out.println(anogram[i] + anogram[j]);
+        String[] text = getStringText.split(" ");
+        String[] anagram = text.clone();
+
+        for (int i = 0, n = text.length; i < n; i++) {
+            char[] a = text[i].toCharArray();
+            Arrays.sort(a);
+            anagram[i] = Arrays.toString(a);
+        }
+        for (int i = 0, n = text.length; i< n; i++){
+            for (int j = i + 1; j < n; j++){
+                if (anagram[i].equals(anagram[j])){
+                    System.out.println(text[i] + " - " + text[j]);
                 }
             }
         }
     }
-
-    static String getString(){
-        String getStringText = scanner.nextLine();
-
-        return getStringText;
-    }
 }
+
